e86533df19c9fcf668fca11629ff60a136776084
amithg2 <amitadi91@gmail.com>
2022-02-02 11:56:12 +0200

    first test

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const axios = require('axios')
+const axios = require("axios");
 
+const fetchData = async () => {
+  const { data } = await axios.get("https://api.github.com/repos/amithg2/github-demo/commits");
+  console.log(data);
+};
+
+fetchData()

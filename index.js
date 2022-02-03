2ca1b633a0f6bd6743ddac0e06c9ef6b8d0480af
amithg2 <amitadi91@gmail.com>
2022-02-03 19:14:03 +0200

    changed from post to get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const sha = process.argv[2]
 const sendCommitNotification = async () => {
   try {
-    await axios.post(`https://auditech-server.herokuapp.com/commits/new/${sha}`, );
+    await axios.get(`https://auditech-server.herokuapp.com/commits/new/${sha}`, );
     console.log("sending message to server - success");
   } catch (e) {
     console.log("sending message to server - ERROR");
